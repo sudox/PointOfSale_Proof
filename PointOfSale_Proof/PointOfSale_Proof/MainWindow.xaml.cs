c6b007e292a17d850f535ab6a6849e0247bc8257
sudox <mjg72@zips.uakron.edu>
2016-03-06 15:32:01 -0500

    Added functions to Transaction, added functionality for sku and upc input. Next step is to add add multiple text boxes for each part of transaction line

--- PointOfSale_Proof/PointOfSale_Proof/MainWindow.xaml.cs
+++ PointOfSale_Proof/PointOfSale_Proof/MainWindow.xaml.cs
@@ -21,20 +21,56 @@ namespace PointOfSale_Proof
 
     public partial class MainWindow : Window
     {
+        private Transaction t;
+
         public MainWindow()
         {
             InitializeComponent();
+            t = new Transaction();
         }
 
-        public void loadItems()
+        public StoreItems checkItems(string input)
         {
-            //Would load items list from SQL database, for now will hard code items in
+            //Would check items in SQL database, for now will hard code items in
+            StoreItems[] i = new StoreItems[] { new StoreItems("2x4-8", 2.94, "204932", "002547182942"), new StoreItems("1/2\" OSB 4x8", 12.24, "125239", "004254843612"), new StoreItems("1x6-8 Premium", 4.52, "854742", "002345859652") };
+            foreach(StoreItems item in i)
+            {
+                if(item.getSKU() == input)
+                {
+                    return item;
+                }
+
+                if(item.getUPC() == input)
+                {
+                    return item;
+                }
+            }
+
+            return null;
         }
 
         public void line_Input(object sender, RoutedEventArgs e)
         {
             //Will check to see if enter was pressed, then logic for determining line items
-            StoreItems items = new StoreItems();
+            if(Keyboard.IsKeyDown(Key.Enter))
+            {
+                string input = lineInput.Text;
+                lineInput.Text = "";
+
+                StoreItems lineItem = checkItems(input);
+                if(lineItem != null)
+                {
+                    //TODO Add to transaction
+                    
+                    //Add items to screen
+                    lineItems.Content += input;
+                    lineItems.Content += "\t\t\t\t\t";
+                    lineItems.Content += lineItem.getName();
+                    lineItems.Content += "\t\t\t\t\t$";
+                    lineItems.Content += Convert.ToString(lineItem.getPrice());
+                    lineItems.Content += "\n";
+                }
+            }
         }
     }
 }
--- PointOfSale_Proof/PointOfSale_Proof/Transaction.cs
+++ PointOfSale_Proof/PointOfSale_Proof/Transaction.cs
@@ -9,21 +9,59 @@ namespace PointOfSale_Proof
     class Transaction
     {
         private int transID;
-        private StoreItems[] items;
+        private List<StoreItems> items;
         private double total;
 
         public Transaction()
         {
             transID = 0;
-            items = new StoreItems[1];
+            items = new List<StoreItems>();
             total = 0.0;
         }
 
-        public Transaction(int i, StoreItems[] l, int t)
+        public Transaction(int i, List<StoreItems> l, int t)
         {
             transID = i;
             items = l;
             total = t;
+        }
+
+        public int getTransactionID()
+        {
+            return transID;
+        }
+
+        public void setTransactionID(int i)
+        {
+            transID = i;
+        }
+
+        public List<StoreItems> getItems()
+        {
+            return items;
+        }
+
+        public void addItemList(List<StoreItems> l)
+        {
+            items = l;
+        }
+
+        public StoreItems getItem(int location)
+        {
+            if (items.Count > location)
+            {
+                return items.ElementAt(location);
+            }
+
+            else
+            {
+                return null;
+            }
+        }
+
+        public void addItem(StoreItems i)
+        {
+            items.Add(i);
         }
     }
 }
